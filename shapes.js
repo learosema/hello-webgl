e251bf4183b878d35cf5c85c97b2e0a3e7e4db5b
Lutz Rosema <info@terabaud.de>
2015-08-13 02:02:56 +0200

    do not prefix

--- shapes.js
+++ shapes.js
@@ -4,35 +4,34 @@ g=a.getContext('webgl')||a.getContext('experimental-webgl')
 g.enable(g.DEPTH_TEST)
 g.depthFunc(g.LEQUAL)
 
-$prog(g, [vertexShader, fragmentShader])
-pos = $attr("pos")
-triBuf = $buf([0,1,0,-1,-1,0,1,-1,0])
-quadBuf = $buf([1,1,0,-1,1,0,1,-1,0,-1,-1,0])
-uP=perspective(45,w/h,0.1,100)
-uMV=mI()
-
-
+$prog(g,[vertexShader,fragmentShader])
+pos=$attr("pos")
+triBuf=$buf([0,1,0,-1,-1,0,1,-1,0])
+quadBuf=$buf([1,1,0,-1,1,0,1,-1,0,-1,-1,0])
+P=perspective(45,w/h,0.1,100)
+MV=mI()
 
 ~function scene(time) {
 	time/=1e3
+	// function to set the uniforms
 	unis=function(){
-		$uni("time", time)
-		$uniV("resolution", [w,h])
-		$uniM("uMV", uMV)
-		$uniM("uP", uP)
+		$uni("time",time)
+		$uniV("resolution",[w,h])
+		$uniM("MV",MV)
+		$uniM("P",P)
 	}
 	// clear screen
-	g.clearColor(1/5, 1/5, 1/5, 1)
-	g.clear(g.COLOR_BUFFER_BIT | g.DEPTH_BUFFER_BIT)
+	g.clearColor(1/5,1/5,1/5,1)
+	g.clear(g.COLOR_BUFFER_BIT|g.DEPTH_BUFFER_BIT)
 
 	// draw trigon
-	uMV=mT(-1.5,0,-7)
+	MV=mT(-1.5,0,-7)
 	$bind(pos,triBuf,3)
 	unis()
 	g.drawArrays(g.TRIANGLE_STRIP,0,3)
 
 	// draw square
-	uMV=mT(1.5,0,-7)
+	MV=mT(1.5,0,-7)
 	$bind(pos,quadBuf,3)
 	unis()
 	g.drawArrays(g.TRIANGLE_STRIP,0,4)
