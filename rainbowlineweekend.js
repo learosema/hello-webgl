c3d8e9c3ff98083168eb71317d7c2357fb69a4f4
Lutz Rosema <terabaud@gmail.com>
2015-11-13 01:41:30 +0100

    working on it

--- rainbowlineweekend.js
+++ rainbowlineweekend.js
@@ -22,19 +22,17 @@ P=perspective(45,w/h,0.1,1000)
 p2=$prog(g,[vs2,fs2],["pos"])
 bkgBuf=$buf(sq())
 $bind("pos",bkgBuf,3)
-~function scene(time) {
+~function scene(time,m) {
 	time/=1e3
-	$prog(g,p1)
-	$bind("pos",diceBuf,3)
-	$bind("color",colorBuf,3)
 	// clear screen
 	g.clearColor(1/5,1/5,1/5,1)
 	g.clear(g.COLOR_BUFFER_BIT|g.DEPTH_BUFFER_BIT)
 	// create transformation/rotation matrix
-	// mX is matrix multiplication, mT is transformation, mR* is rotation 
-	M=mX(P,mX(mT(sin(time*2)*3,cos(time*3)*1.5,sin(time)*5-10),
+	// mX is matrix multiplication, mT is transformation, mR* is rotation
+	m=mX(P,mX(mT(sin(time*2)*3,cos(time*3)*1.5,sin(time)*5-10),
 	     mX(mX(mRx(time*3.5),mRy(time*2)),mRz(time*1.6))))
-
+	// program 1
+	$prog(g,p1,{time:time,rez:[w,h],m:M},{pos:[diceBuf,3],color:[colorBuf,3]})
 	// set uniforms
 	$uni("time",time)
 	$uniV("resolution",[w,h])
