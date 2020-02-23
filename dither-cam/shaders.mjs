3d0c8ec5e55ee4da5255b78121f7a246efb20641
Lea Rosema <terabaud@gmail.com>
2020-02-23 19:36:10 +0100

    feat: add luminance dithering strategy

--- dither-cam/shaders.mjs
+++ dither-cam/shaders.mjs
@@ -51,21 +51,57 @@ vec2 getTexCoords(vec2 position) {
   return 1.0 - position.xy / vec2(width, height);
 }
 
+// dumb closest color strategy
 vec4 closestColor(vec4 color) {
   return vec4(
-    clamp(floor(color.x * 8. + .5) * 42.5, 0., 255.) / 255.,
-    clamp(floor(color.y * 8. + .5) * 42.5, 0., 255.) / 255.,
-    clamp(floor(color.z * 8. + .5) * 42.5, 0., 255.) / 255.,
+    clamp(floor(color.x * 4. + .5) * 85., 0., 255.) / 255.,
+    clamp(floor(color.y * 4. + .5) * 85., 0., 255.) / 255.,
+    clamp(floor(color.z * 4. + .5) * 85., 0., 255.) / 255.,
     1.
   );
 }
 
+// luminance strategy: (TODO)
+float luminance(vec4 color) {
+  return 0.2126*color.x + 0.7152*color.y + 0.0722*color.z;
+}
+
+vec4 getEgaColor(int index) {
+  float fi = float(index);
+  float b = clamp((2. * mod(fi, 2.) + mod(floor(fi / 8.), 2.)) * (1./3.), 0., 1.);
+  float g = clamp((2. * mod(floor(fi / 2.), 2.) + mod(floor(fi / 16.), 2.)) * (1./3.), 0., 1.);
+  float r = clamp((2. * mod(floor(fi / 4.), 2.) + mod(floor(fi / 32.), 2.)) * .333, 0., 1.);
+  return vec4(r, g, b, 1.);
+}
+
+vec4 closestColorLum(vec4 color) {
+  float l = luminance(color);
+  float lResult = 9999.0;
+  vec4 result = vec4(0.);
+  for (int i = 0; i < 64; i++) {
+    vec4 colorI = getEgaColor(i);
+    float lI = luminance(colorI);
+    if (abs(lI - l) < abs(lResult - l)) {
+      lResult = lI;
+      result = colorI;
+    }
+  }
+  return result;
+}
+
+
+// hue distance strategy (TODO)
+
+
+
 void main() {
   vec2 p = normalizeScreenCoords();
   vec2 coord = 1.0 - gl_FragCoord.xy / vec2(width, height);
-  gl_FragColor = step(
+  bool isEven = mod(gl_FragCoord.x + gl_FragCoord.y, 2.) < 1.;
+  gl_FragColor = closestColor(step(
     texture2D(texture0, gl_FragCoord.xy / 8.).r,
-    closestColor(texture2D(texture1, coord))
-  );
+    isEven ? closestColor(texture2D(texture1, coord)) : 
+             closestColorLum(texture2D(texture1, coord))
+  ));
 }
 `;
